a6c976cd6bd8583702d321e16d8b235651db0d46
Eric Leblond <eric@regit.org>
2016-07-31 22:48:21 +0200

    WIP4

--- screens/tides/screen.py
+++ screens/tides/screen.py
@@ -1,6 +1,15 @@
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen
+import requests
+import time
 
 class TidesScreen(Screen):
+    "https://www.worldtides.info/api?extremes&lat={lat}&lon={lon}&length=1209600&maxcalls=5&key={key}"
+    forecast = "http://api.wunderground.com/api/{key}/forecast/q/{location}"
+
     def __init__(self, **kwargs):
         super(TidesScreen, self).__init__(**kwargs)
+        # Init data by checking cache then calling API
+
+    def get_data(self):
+            self.forecast = requests.get(self.url_forecast).json()
