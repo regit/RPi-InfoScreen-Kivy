05f7ca24cf455a0b17a84d0e6ad1389bdb93ffc8
Eric Leblond <eric@regit.org>
2016-07-31 22:04:24 +0200

    WIP2

---
+++ screens/tides/screen.py
@@ -0,0 +1,6 @@
+from kivy.uix.label import Label
+from kivy.uix.screenmanager import Screen
+
+class TidesScreen(Screen):
+    def __init__(self, **kwargs):
+        super(TidesScreen, self).__init__(**kwargs)
